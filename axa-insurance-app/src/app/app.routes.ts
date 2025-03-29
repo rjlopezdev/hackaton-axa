24d0c71b9cf0b259c3f8f82986e54c83a15ced68
nina-osk <ing.oscarina@gmail.com>
2025-03-29 12:31:56 +0100

    Home page and login

--- axa-insurance-app/src/app/app.routes.ts
+++ axa-insurance-app/src/app/app.routes.ts
@@ -1,3 +1,37 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './features/auth/login/login.component';
+import { RegisterComponent } from './features/auth/register/register.component';
+import { HomePageComponent } from './features/home/home-page/home-page.component';
+import { DashboardComponent } from './features/dashboard/dashboard/dashboard.component';
+import { InsuranceListComponent } from './features/insurance/insurance-list/insurance-list.component';
+import { InsuranceDetailComponent } from './features/insurance/insurance-detail/insurance-detail.component';
+import { AuthGuard } from './core/guards/auth.guard';
 
-export const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { 
+    path: 'dashboard', 
+    component: DashboardComponent, 
+    canActivate: [AuthGuard] 
+  },
+  { 
+    path: 'insurance', 
+    component: InsuranceListComponent, 
+    canActivate: [AuthGuard] 
+  },
+  { 
+    path: 'insurance/:id', 
+    component: InsuranceDetailComponent, 
+    canActivate: [AuthGuard] 
+  },
+  { path: '**', redirectTo: '' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
