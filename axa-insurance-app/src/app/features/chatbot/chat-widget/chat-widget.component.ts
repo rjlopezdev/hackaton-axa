ba2171da62fa6da187327d6e9de2dc31cb72edce
Raúl Julián López Caña <rjlopezmad@gmail.com>
2025-03-29 12:14:38 +0100

    feat

---
+++ axa-insurance-app/src/app/features/chatbot/chat-widget/chat-widget.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-widget',
+  standalone: true,
+  imports: [],
+  templateUrl: './chat-widget.component.html',
+  styleUrl: './chat-widget.component.scss'
+})
+export class ChatWidgetComponent {
+
+}
